9693ca572a513ad51063fabfe6e813a3382ff261
xtrp <36894883+xtrp@users.noreply.github.com>
2021-07-17 23:37:15 -0400

    Minor design fix

--- src/components/ErrorMessage/index.js
+++ src/components/ErrorMessage/index.js
@@ -1,4 +1,4 @@
 const ErrorMessage = ({ displayed, children }) => {
-  return <p style={{ textAlign: 'center', fontSize: '1.15rem', color: 'var(--red-color)', fontWeight: 600 }}>{children}</p>;
+  return <p style={{ textAlign: 'center', fontSize: '1.15rem', color: 'var(--red-color)', fontWeight: 600, marginTop: '1.5rem' }}>{children}</p>;
 };
 export default ErrorMessage;
